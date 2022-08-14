b3b5cefb0827fa7728622fee4fad5776a763c329
frank-booth <jfraley519@gmail.com>
2022-08-14 11:52:04 -0400

    update rider working

--- routes/RiderRouter.js
+++ routes/RiderRouter.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/RiderController')
 Router.get('/', controller.getAllRiders)
 Router.get('/:rider_id', controller.getRiderById)
 Router.post('/', controller.createNewRider)
+Router.put('/:rider_id', controller.updateRider)
 
 module.exports = Router
